5cf5b6c674d2cd8a86453338000bee1cea30d8da
Jeevan1351 <vijayeshjeevan.cs19@bmsce.ac.in>
2021-06-19 18:12:09 +0530

    get_grades

--- Back-end/sql-connect/index.js
+++ Back-end/sql-connect/index.js
@@ -13,7 +13,7 @@ var connection = mysql.createConnection({
 connection.connect();
 var id = "12344"
 var role = "student"
-var sql = `select * from login where role = "Student"`;
+var sql = `select c.course_id, c.course_name, c.credits, c.course_semester, c.course_department, m.m_course_id, m.cie1, m.cie2, m.cie3, m.lab, m.internal, m.see from student s, marks m, courses c where s.g_id = "108960148661406427027" and s.usn = m.m_usn and m.m_course_id = c.course_id;`
 
 var query = connection.query(sql, function(err, result){
     if(err) console.log(err)
